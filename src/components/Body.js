81ea5024ef6152bc51dab5c59cb1b64a44307406
Troy <troyakins99@yahoo.com>
2022-01-18 22:27:00 -0600

    updated bio text and image

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,11 +4,11 @@ export const Body = () => {
     return (
         <div className='body'>
             <div>
-                <img src="https://images.unsplash.com/photo-1610827255424-0ed21ae4f50f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"></img>
+                <a href="https://www.behance.net/gallery/135409745/Spoiled-Stop-Food-Waste-Case-Study" target="_blank"><img src="https://live.staticflickr.com/65535/51830150915_09141d600e_b.jpg" alt="an image of a cell phone displaying a fictional app called 'spoiled' which is a project to eliminate food waste."></img></a>
             </div>
             <div>
                 <h1>Hi! My name is Troy.</h1>
-                <p>I uncover design solutions to complex problems by reimagining pain points into usable and enriching experiences. I craft interactions and web applications for the users of tomorrow with the tools I have today. </p>
+                <p> I'm a Texas-based UX designer and engineer. I take ideas, problems, and needs and transform them into enriching experiences! I craft interactions and applications for the users of tomorrow with the tools I have today.</p>
                 <p>↦ <a href="https://www.behance.net/troydomio" target="_blank">Projects</a></p>
                 <p>↦ <a href="https://drive.google.com/file/d/1GXFuYOXGvEVW9LXBQtJYg9jmokkgVanh/view" target="_blank">Resume</a></p>
                 <p>troy_domio@yahoo.com</p>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,11 +4,11 @@ export const Footer = () => {
     return (
         <div className='footer'>
             <div className='socials'>
-                <a href="https://github.com/troydomio/.design" target="_blank"><img src="https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/042017/untitled-2_5.png?itok=IlsUnu37" alt="github"/></a>
+                <a href="https://github.com/troydomio/.design" target="_blank"><img src="https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/042017/untitled-2_5.png?itok=IlsUnu37" alt="github logo image"/></a>
 
-                <a href="https://www.linkedin.com/in/troydomio/" target="_blank"><img src="https://cdn3.iconfinder.com/data/icons/free-social-icons/67/linkedin_circle_black-512.png" alt="linkedin"/></a>
+                <a href="https://www.linkedin.com/in/troydomio/" target="_blank"><img src="https://cdn3.iconfinder.com/data/icons/free-social-icons/67/linkedin_circle_black-512.png" alt="linkedin logo image"/></a>
 
-                <a href='https://twitter.com/TroyDomio' target="_blank"><img src="https://i.pinimg.com/originals/fc/6c/5e/fc6c5e3c36506985097f7238de63871d.jpg" alt="twitter"/></a>
+                <a href='https://twitter.com/TroyDomio' target="_blank"><img src="https://i.pinimg.com/originals/fc/6c/5e/fc6c5e3c36506985097f7238de63871d.jpg" alt="twitter logo image"/></a>
             </div>
             <div className='credit'>
             Build with ❤️ by Troy Domio using <a href="https://reactjs.org/" target="_blank">React</a> and <a href="https://www.w3.org/Style/CSS/Overview.en.html" target="_blank">CSS</a>
